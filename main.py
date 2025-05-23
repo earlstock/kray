f1608e585c70098b1117ba1cf9ecccf0f030f0ba
28stockr <28stockr@bwschools.net>
2025-05-23 09:16:13 -0400

    comit

--- main.py
+++ main.py
@@ -10,37 +10,49 @@ t.speed(0)
 
 #sand
 t.penup()
-t.goto(-5000,-100)
+t.goto(-5000,200)
 t.pendown()
-t.fillcolor("navajowhite2")
-t.pencolor("navajowhite2")
+t.fillcolor("green")
+t.pencolor("green")
 t.begin_fill()
 t.goto(5000,-100)
 t.goto(5000,-5000)
 t.goto(-5000,-5000)
 t.goto(-5000,-100)
 t.end_fill()
-
+#danny brown
 t.penup()
-t.goto(50,0)
+t.goto(150,0)
 t.pendown()
 t.pencolor("navajowhite3")
 t.fillcolor("navajowhite3")
 t.begin_fill()
-t.goto(150,0)
-t.goto(100,100)
+t.goto(-200,0)
+t.goto(2,100)
 t.goto(150,0)
 t.end_fill()
 
 
+#sun
+t.penup()
+t.goto(-300,300)
+t.pendown()
+t.circle(70)
+t.fillcolor("yellow")
+t.pencolor("yellow")
+t.begin_fill()
+t.end_fill()
 
 
+#flower
+t.penup()
+t.goto(190,0)
+t.pendown()
+t.pencolor("green")
+t.fillcolor("green")
 
-
-
-
-
-
+t.goto(200,30)
+t.goto(200,40)
 
 
 
